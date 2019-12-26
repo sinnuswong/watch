84a5223b2a2554a5de6b086999773d38f93c69db
kuoer.wang <kuoer.wang@samgung.com>
2019-12-26 23:39:10 +0800

    add Home

---
+++ Home/inc/home.h
@@ -0,0 +1,19 @@
+#ifndef __home_H__
+#define __home_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "home"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.eros.home"
+#endif
+
+#endif /* __home_H__ */
