ad5ddf68ef786cdf0c120556436aa9573af58fa3
kuoer.wang <kuoer.wang@samgung.com>
2020-01-01 14:20:08 +0800

    fixz

--- Debug/.d
+++
@@ -1,7 +0,0 @@
-res/edje/main.edj: \
-  ../edje/images/b_detail_bg_cover.png \
-  ../edje/images/b_buddy_list_focus_bg.png \
-  ../edje/images/wc_contact_bg.png \
-  ../edje/images/wc_contact_detail_bg.png \
-  ../edje/images/wc_contact_detail_call_icon.png \
-  ../edje/images/wc_contact_detail_msg_icon.png
